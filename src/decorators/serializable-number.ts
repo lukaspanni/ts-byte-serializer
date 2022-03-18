3db6b9b524ddfefb618404dc0cdbfbdf69daf70d
Lukas Panni <lukas-panni@web.de>
2022-03-18 10:32:45 +0100

    Test: Add integration test for simple serializable
    - tests serializable decorated class with only uint properties

--- src/decorators/serializable-number.ts
+++ src/decorators/serializable-number.ts
@@ -1,19 +1,20 @@
-import { SerializablePrimitive } from "../serializable-primitives";
-import { serializablePropertyPrefix } from "./serializable";
+import { SerializablePrimitive } from '../serializable-primitives';
+import { serializablePropertyPrefix } from './serializable';
 
 /**
  * Decorator to include a number-property inside a serializable-decorated class in its byte-representation
  * @param type type of the number (uint8/16/32/64 or int8/16/32/64)
  */
-export const SerializableNumber = (type: { new(value: number | bigint): SerializablePrimitive<number | bigint> }) => {
-  return function(target: any, propertyKey: string) {
+export const SerializableNumber = (type: { new (value: number): SerializablePrimitive<number> }) => {
+  //TODO: find way to support bigints -> maybe with separate decorator?
+  return function (target: any, propertyKey: string) {
     const primitiveObject = new type(0);
     Object.defineProperty(target, propertyKey, {
       enumerable: true,
       get: () => {
         return primitiveObject.value;
       },
-      set: (value: number | bigint) => {
+      set: (value: number) => {
         primitiveObject.value = value ?? 0;
       }
     });
---
+++ tests/integration/simple-serializable-class.spec.ts
@@ -0,0 +1,49 @@
+import { Serializable as ISerializable, AppendableByteStream } from '../../src/serializable';
+import { Serializable } from '../../src/decorators/serializable';
+import { Uint8, Uint16, Uint32, SerializableNumber } from '../../src/index';
+
+@Serializable({ littleEndian: true })
+class ExampleClass implements ISerializable {
+  @SerializableNumber(Uint8)
+  public testByte: number = 0;
+
+  @SerializableNumber(Uint16)
+  public testUShort: number = 0;
+
+  @SerializableNumber(Uint32)
+  public testUInt: number = 0;
+
+  constructor(byte: number = 0, ushort: number = 0, uint: number = 0) {
+    this.testByte = byte;
+    this.testUShort = ushort;
+    this.testUInt = uint;
+  }
+
+  public serialize(): Uint8Array {
+    throw new Error('Method not implemented.');
+  }
+  public deserialize(bytes: AppendableByteStream): void {
+    throw new Error('Method not implemented.');
+  }
+}
+
+describe('Serializable class with primitives only', () => {
+  it('object should serialize into Uint8Array', () => {
+    const obj = new ExampleClass(42, 2048, 131_072);
+    const expectedBytes = new Uint8Array([0x2a, 0x00, 0x08, 0x00, 0x00, 0x02, 0x00]);
+
+    const serialized = obj.serialize();
+    expect(serialized).toEqual(expectedBytes);
+  });
+
+  it('deserialize should return object', () => {
+    const bytes = new Uint8Array([0x81, 0x88, 0x27, 0xb3, 0xa7, 0x2f, 0x00]);
+    const expectedObj = new ExampleClass(129, 10_120, 3_123_123);
+
+    //TODO: allow deserialize with Uint8Array directly
+    const obj = new ExampleClass();
+    //TODO: add deserialize function that returns an object
+    obj.deserialize({ view: new DataView(bytes.buffer), pos: 0, littleEndian: true });
+    expect(obj).toEqual(expectedObj);
+  });
+});
