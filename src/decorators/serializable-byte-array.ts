4be48a08b0a0bf4839ccd0ebe7f7dd44d41d6c7b
Lukas Panni <lukas-panni@web.de>
2022-03-30 08:47:37 +0200

    Doc: Improve doc-comments

--- src/decorators/serializable-byte-array.ts
+++ src/decorators/serializable-byte-array.ts
@@ -3,11 +3,11 @@ import { serializablePropertyPrefix } from './serializable';
 
 /**
  * Decorator to include a uint8 property inside a serializable-decorated class in its byte-representation
+ * @param lengthParameter: optionally specify either a fixed length or the name of a property to use as array-length during deserialization
  */
-
 export const SerializableByteArray = (lengthParameter?: number | string): Function => {
   return function (target: any, propertyKey: string) {
-    let primitiveObject = new ByteArray(undefined, lengthParameter);
+    const primitiveObject = new ByteArray(undefined, lengthParameter);
     Object.defineProperty(target, serializablePropertyPrefix + propertyKey, {
       value: primitiveObject,
       enumerable: true
