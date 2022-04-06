f00ab8cd8d0d1b799328b49e7887c279ff018097
Lukas Panni <lukas-panni@web.de>
2022-04-06 09:49:24 +0200

    Test: Update integration tests to include floats

--- tests/integration/simple-serializable-class.spec.ts
+++ tests/integration/simple-serializable-class.spec.ts
@@ -8,6 +8,7 @@ import {
   SerializableClass,
   AppendableByteStream
 } from '../../src/index';
+import { Float32, Float64 } from '../../src/serializable-primitives/serializable-number';
 
 @SerializableClass({ littleEndian: true })
 class ExampleClass implements Serializable {
@@ -23,11 +24,26 @@ class ExampleClass implements Serializable {
   @SerializableNumber(Uint64)
   public testULong: bigint = BigInt(0);
 
-  constructor(byte: number = 0, ushort: number = 0, uint: number = 0, ulong: bigint = BigInt(0)) {
+  @SerializableNumber(Float32)
+  public testFloat: number = 0.0;
+
+  @SerializableNumber(Float64)
+  public testDouble: number = 0.0;
+
+  constructor(
+    byte: number = 0,
+    ushort: number = 0,
+    uint: number = 0,
+    ulong: bigint = BigInt(0),
+    float: number = 0.0,
+    double: number = 0.0
+  ) {
     this.testByte = byte;
     this.testUShort = ushort;
     this.testUInt = uint;
     this.testULong = ulong;
+    this.testFloat = float;
+    this.testDouble = double;
   }
 
   public serialize(): Uint8Array {
@@ -40,9 +56,10 @@ class ExampleClass implements Serializable {
 
 describe('Serializable class with primitives only', () => {
   it('object should serialize into Uint8Array', () => {
-    const obj = new ExampleClass(42, 2048, 131_072, BigInt(4_000_000_000));
+    const obj = new ExampleClass(42, 2048, 131_072, BigInt(4_000_000_000), 13.42, 15.123);
     const expectedBytes = new Uint8Array([
-      0x2a, 0x00, 0x08, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0xee, 0x6b, 0x28, 0x00
+      0x2a, 0x00, 0x08, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0xee, 0x6b, 0x28, 0x00, 0x52, 0xb8, 0x56, 0x41,
+      0xe5, 0xd0, 0x22, 0xdb, 0xf9, 0x3e, 0x2e, 0x40
     ]);
 
     const serialized = obj.serialize();
@@ -51,9 +68,10 @@ describe('Serializable class with primitives only', () => {
 
   it('deserialize should return object', () => {
     const bytes = new Uint8Array([
-      0x81, 0x88, 0x27, 0xb3, 0xa7, 0x2f, 0x00, 0x2a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+      0x81, 0x88, 0x27, 0xb3, 0xa7, 0x2f, 0x00, 0x2a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x52, 0xb8, 0x56, 0x41,
+      0xe5, 0xd0, 0x22, 0xdb, 0xf9, 0x3e, 0x2e, 0x40
     ]);
-    const expectedObj = new ExampleClass(129, 10_120, 3_123_123, BigInt(42));
+    const expectedObj = new ExampleClass(129, 10_120, 3_123_123, BigInt(42), 13.42, 15.123);
 
     //TODO: allow deserialize with Uint8Array directly
     const obj = new ExampleClass();
